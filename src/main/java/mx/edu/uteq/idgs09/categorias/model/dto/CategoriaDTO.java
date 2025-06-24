6224d8052c08f818f65184f0742794c0d5277a9b
Litzi-Otero <2022371132@uteq.edu.mx>
2025-06-24 00:43:54 -0600

    feat: creacion de dto

--- src/main/java/mx/edu/uteq/idgs09/categorias/model/dto/CategoriaDTO.java
+++ src/main/java/mx/edu/uteq/idgs09/categorias/model/dto/CategoriaDTO.java
@@ -1,5 +1,18 @@
 package mx.edu.uteq.idgs09.categorias.model.dto;
 
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class CategoriaDTO {
-    
+    private int id;
+    private String nombre;
+    private String nombreCategoriaAnterior;
+    private String nombreCategoriaFederal;
+    private String nombreCategoriaEstatal;
+    private boolean activo;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaActualizacion;
+    private List<RequisitoDTO> requisitos;
 }
